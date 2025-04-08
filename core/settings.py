20585d2f0594a2936e6e6dd246ab9f9df9a093a1
ananthuuu <ananthcjayan12@gmail.com>
2025-04-08 17:28:47 +0530

    add cors above all

--- core/settings.py
+++ core/settings.py
@@ -58,10 +58,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  # CORS middleware - add before CommonMiddleware
+    'django.contrib.sessions.middleware.SessionMiddleware',  # CORS middleware - add before CommonMiddleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
